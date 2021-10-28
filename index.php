e5a4bf5828f081977fbd789f1d30802ed019e5cc
Nguyễn Thu Phương <90679641+thuphuong27@users.noreply.github.com>
2021-10-28 16:28:22 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.6.1/font/bootstrap-icons.css">
+
+
+</head>
+
+<body id="body-pd">
+
+        <header class="header" id="header">
+            <div class="header_toggle"> <i class='bi bi-list' id="header-toggle"></i> </div>
+            <div class="header_img"> <img src="https://w7.pngwing.com/pngs/518/320/png-transparent-computer-icons-mobile-app-development-android-my-account-icon-blue-text-logo.png" alt="avatar"> </div>
+        </header>
+        <div class="l-navbar" id="nav-bar">
+            <nav class="nav">
+                <div> <a href="#" class="nav_logo"> <i class="bi bi-stack nav_logo-icon"></i> <span class="nav_logo-name">High School</span> </a>
+                    <div class="nav_list">
+                        <a href="#" class="nav_link active">
+                            <i class='bi bi-house-door nav_logo-icon'></i>
+                            <span class="nav_name">Trang chủ</span> </a>
+                        <a href="#" class="nav_link">
+                            <i class='bi bi-person-lines-fill nav_logo-icon'></i>
+                            <span class="nav_name">Thông tin Giáo Viên</span> </a>
+                        <a href="#" class="nav_link">
+                            <i class='bi bi-upload nav_logo-icon'></i>
+                            <span class="nav_name">Cập nhật điểm của học sinh</span> </a>
+                        <a href="#" class="nav_link">
+                            <i class='bi bi-calendar3 nav_logo-icon'></i>
+                            <span class="nav_name">Lịch giảng dạy</span> </a>
+                        <a href="#" class="nav_link">
+                            <i class='bi bi-menu-button nav_logo-icon'></i>
+                            <span class="nav_name">Danh sách học sinh trong lớp</span> </a>
+                        <a href="#" class="nav_link">
+                            <i class='bi bi-menu-button-fill nav_logo-icon'></i>
+                            <span class="nav_name">Danh sách giáo viên dạy lớp</span> </a>
+                    </div>
+                </div> 
+                <a href="#" class="nav_link"> <i class='bi bi-box-arrow-right nav_logo-icon '></i> <span class="nav_name">SignOut</span> </a>
+            </nav>
+        </div>
+       
+
+        <script src="main.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    </body>
+
+</html>
