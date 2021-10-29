de22197d74c71f7bfb2ba4ccb752ba5572cffd4d
NguyenMam <condien852@gmail.com>
2021-10-29 14:32:54 +0700

    01

---
+++ dsgv.php
@@ -0,0 +1,28 @@
+<?php include 'gv/gvcn.php' ?>
+<div class="text-center">
+    <div class="container">
+        <h1><b>Danh sách giáo viên dạy lớp</b></h1>
+        <br><br>
+    </div>
+</div>
+<br><br>
+<div class="table-responsive">
+    <table class="table table-striped table-hover">
+        <thead>
+            <tr>
+                <th scope="col">STT</th>
+                <th scope="col">Tên giáo  viên</th>
+                <th scope="col">Môn học</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th scope="row">1</th>
+                <td>...</td>
+                <td>...</td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+<a href="#" class="btn btn-success">Cập nhật danh sách</a>
+<?php include 'gv/footer.php' ?>
---
+++ dshs.php
@@ -0,0 +1,28 @@
+<?php include 'gv/gvcn.php' ?>
+<div class="text-center">
+    <div class="container">
+        <h1><b>Danh sách học sinh trong lớp</b></h1>
+        <br><br>
+    </div>
+</div>
+<br><br>
+<div class="table-responsive">
+    <table class="table table-striped table-hover">
+        <thead>
+            <tr>
+                <th scope="col">STT</th>
+                <th scope="col">Mã học sinh</th>
+                <th scope="col">Họ tên học sinh</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th scope="row">1</th>
+                <td>...</td>
+                <td>...</td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+<a href="#" class="btn btn-success">Cập nhật danh sách</a>
+<?php include 'gv/footer.php' ?>
